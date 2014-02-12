35460d1411b0ae67f111772c095df897d97e9b5c
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2014-02-12 15:53:54 +0530

    made changes in directory

--- src/com/webonise/gcmdemo/MainActivity.java
+++ src/com/webonise/gcmdemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.webonise.gcmdemo;
+package com.webonise.gcmdemo; 
 
 import android.app.Activity;
 import android.os.Bundle;
---
+++ src/com/webonise/gcmdemo/gcm/GCMIntentService.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.webonise.gcmdemo.gcm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+import com.google.android.gcm.GCMRegistrar;
+import com.webonise.gcmdemo.MainActivity;
+import com.webonise.gcmdemo.R;
+import com.webonise.gcmdemo.R.string;
+
+/**
+ * IntentService responsible for handling GCM messages.
+ */
+public class GCMIntentService extends GCMBaseIntentService {
+
+    @SuppressWarnings("hiding")
+    private static final String TAG = "GCMIntentService";
+
+    public GCMIntentService() {
+        super(MainActivity.SENDER_ID);
+    }
+
+    @Override
+    protected void onRegistered(Context context, String registrationId) {
+        Log.i(TAG, ">>>>>>>>>>>>>>>>> Device registered: regId = " + registrationId);
+      
+    }
+
+    @Override
+    protected void onUnregistered(Context context, String registrationId) {
+        Log.i(TAG, ">>>>>>>>>>> Device unregistered");
+        if (GCMRegistrar.isRegisteredOnServer(context)) {
+            //un register from server if needed.
+        } else {
+            // This callback results from the call to unregister made on
+            // ServerUtilities when the registration to the server failed.
+            Log.i(TAG, "Ignoring unregister callback");
+        }
+    }
+
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        Log.i(TAG, ">>>>>>>> Received message");
+        String message = getString(R.string.gcm_message); 
+        // notifies user
+    }
+
+    @Override
+    protected void onDeletedMessages(Context context, int total) {
+        Log.i(TAG, ">>>>>>>> Received deleted messages notification");
+        String message = getString(R.string.gcm_deleted, total);
+        // notifies user
+    }
+
+    @Override
+    public void onError(Context context, String errorId) {
+        Log.i(TAG, ">>>>>>>> Received error: " + errorId);
+    }
+
+    @Override
+    protected boolean onRecoverableError(Context context, String errorId) {
+        // log message
+        Log.i(TAG, ">>>>>>>> Received recoverable error: " + errorId);
+        return super.onRecoverableError(context, errorId);
+    }
+}
